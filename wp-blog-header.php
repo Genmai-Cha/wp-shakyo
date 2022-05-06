197857931054dca7fc8b45bbc491f5a9f9d7ba7f
Genmai <hotaru103bsb@gmail.com>
2022-05-06 20:53:57 +0900

    wp-blog-header.phpを書いた

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -4,3 +4,17 @@
  * 
  * @package WordPress
  */
+//もし$wp_did_headerが定義されていなかったら
+if(! isset($wp_did_header)){
+
+    $wp_did_header = true;
+
+    //Load the WordPress Library
+    require_once __DIR__ . '/wp-loade.php';
+
+    //Set up the WordPress query
+    wp();
+
+    //Load the theme template.
+    require_once ABSPATH . WPINC .'/templata-loader.php';
+}
