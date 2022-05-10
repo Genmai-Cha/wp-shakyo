03fd03dbf32f81a2949d08de685840f7e69d1656
Genmai <hotaru103bsb@gmail.com>
2022-05-10 23:37:15 +0900

    ABSPATHが定義されてなかった場合、ファイルディレクトリとして定義する処理を追加

--- wp-load.php
+++ wp-load.php
@@ -2,5 +2,13 @@
 /**
  * Bootstrap file for setting the ABSPATH constant
  * and loading the wp-config.php file.The wp-config.php
- * file will then load the wp-settings.
+ * file will then load the wp-settings.php file,which
+ * will then set up the WordPress eniviroment.
  */
+
+/** Define ABSPATH as the file's directory */ 
+//もし ABSPATHが定義されていなかったら、
+//このファイルのディレクトリのパスをABSPATHと定義する
+if(! defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
+}
