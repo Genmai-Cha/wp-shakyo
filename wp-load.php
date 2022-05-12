4960dd9d0e5660d2e1d2dbe305056810c683e80e
Genmai <hotaru103bsb@gmail.com>
2022-05-12 23:55:42 +0900

    wp-load.phpの最初のコメントアウトを追加（2022.5.12)

--- wp-load.php
+++ wp-load.php
@@ -4,6 +4,16 @@
  * and loading the wp-config.php file.The wp-config.php
  * file will then load the wp-settings.php file,which
  * will then set up the WordPress eniviroment.
+ * 
+ * If the wp-config.php file is not found then an error
+ * will be displayed asking the vistor to set up the
+ * wp-config.php file.
+ * 
+ * Will also search for wp-config.php in WordPress's parent
+ * directory to allow the WordPress directory to remain
+ * untouched
+ * （このファイルの概要を書いてるコメントアウトっぽい）
+ * @package WordPress
  */
 
 /** Define ABSPATH as the file's directory */ 
@@ -12,3 +22,7 @@
 if(! defined('ABSPATH')) {
     define('ABSPATH', __DIR__ . '/');
 }
+
+/*
+* The error_reporting() function can be deisabled in php.ini. 
+*/
